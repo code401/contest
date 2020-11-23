f15dede4314727d7770be1c06e4ec4d01d0a987b
Naziur Rahman <naziurrahman14@gmail.com>
2020-11-23 21:02:23 +0600

    Add files via upload

--- codeforcespractice/1328a.cpp
+++ codeforcespractice/1328a.cpp
@@ -1,15 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+long long int t,a,b;
+cin>>t;
+while(t>0){
+cin>>a>>b;
+if(b>a){
+   cout<<b-a;
+
+}
+else if((a%b)==0){
+    cout<<0;
+}
+else{
 
 
+cout<<(b*((a/b)+1))-a;
 
-
-
-return 0;
 }
 
 
+ t--;
 
 
 
+
+cout<<endl;
+}
+
+return 0;
+}
